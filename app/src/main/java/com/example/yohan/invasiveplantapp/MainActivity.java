04f27fcacb0f81048f1d1c00d4e8bcd102500ce2
yohanym95 <malshikay@gmail.com>
2018-10-19 20:09:28 +0530

    updated interface and updated date spinner

--- app/src/main/java/com/example/yohan/invasiveplantapp/MainActivity.java
+++ app/src/main/java/com/example/yohan/invasiveplantapp/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.yohan.invasiveplantapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
      Spinner spinner1,spinner2,spinner3,spinner4;
      EditText editText;
@@ -18,5 +22,70 @@ public class MainActivity extends AppCompatActivity {
         spinner3 = findViewById(R.id.spinner4);
         spinner4 = findViewById(R.id.spinner5);
         editText = findViewById(R.id.editText2);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.day,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String day = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(),day,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.month,android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter1);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String month = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(),month,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.year,android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter2);
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String year = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(),year,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
+
+ /*   public void onItemSelected(AdapterView<?> parent, View view, int position1, long id) {
+       String day = parent.getItemAtPosition(position1).toString();
+
+    }
+
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    } */
+
+
 }
