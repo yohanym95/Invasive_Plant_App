3cddee1af7b225e453c01ffbfc71ebf314c07402
yohanym95 <malshikay@gmail.com>
2018-10-27 22:28:53 +0530

    added home page

---
+++ app/src/main/java/com/example/yohan/invasiveplantapp/Main2Activity.java
@@ -0,0 +1,51 @@
+package com.example.yohan.invasiveplantapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button button1,button2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button1 = findViewById(R.id.angry_btn);
+        button2 = findViewById(R.id.angry_btn2);
+
+
+
+    }
+
+    public void details(){
+        button1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                      //  Intent i = new Intent(Main2Activity.this,  )
+
+                    }
+                }
+        );
+    }
+
+    public void datacollection(){
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent i1 = new Intent(Main2Activity.this,MainActivity.class);
+                        startActivity(i1);
+
+                    }
+                }
+        );
+    }
+}
